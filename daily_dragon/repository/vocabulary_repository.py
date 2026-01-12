4173ecfab8f61be1952ad97ae231d9e2976a1b40
Oleksandr Havryliuk <oleksander.havryliuk@gmail.com>
2026-01-12 19:36:35 +0200

    #24 draft solution for having different vocabulary files for different users

--- daily_dragon/repository/vocabulary_repository.py
+++ daily_dragon/repository/vocabulary_repository.py
@@ -13,33 +13,35 @@ logger = logging.getLogger(__name__)
 
 
 class VocabularyRepository:
+    USER_VOCABULARY_FILE_SUFFIX = "_vocabulary.json"
 
     def __init__(self):
         self.s3_client = boto3.client('s3')
         self.bucket_name = os.getenv("S3_BUCKET")
-        self.vocabulary_file_key = os.getenv("S3_FILE_PATH")
 
-    def get_vocabulary(self) -> Dict[str, Dict]:
+    def get_vocabulary(self, user_id: str) -> Dict[str, Dict]:
         try:
-            response = self.s3_client.get_object(Bucket=self.bucket_name, Key=self.vocabulary_file_key)
+            key = self._create_key(user_id)
+            response = self.s3_client.get_object(Bucket=self.bucket_name, Key=key)
             return json.loads(response['Body'].read().decode())
         except ClientError as e:
             if e.response['Error']['Code'] == 'NoSuchKey':
                 logger.info("Vocabulary file not found, creating a new one.")
-                return dict()
+                return self._create_new_vocabulary_file(user_id)
             else:
                 logger.error("Error fetching vocabulary file: %s", e)
                 raise
 
-    def save_vocabulary(self, vocabulary: Dict[str, Dict]):
+    def save_vocabulary(self, user_id: str, vocabulary: Dict[str, Dict]):
+        key = f"{user_id}_vocabulary.json"
         self.s3_client.put_object(
             Bucket=self.bucket_name,
-            Key=self.vocabulary_file_key,
+            Key=key,
             Body=json.dumps(vocabulary, ensure_ascii=False).encode('utf-8')
         )
 
-    def add_word(self, word: str) -> None:
-        vocabulary = self.get_vocabulary()
+    def add_word(self, user_id: str, word: str) -> None:
+        vocabulary = self.get_vocabulary(user_id)
 
         if word in vocabulary:
             logger.info("Word already exists in vocabulary: %s", word)
@@ -52,6 +54,24 @@ class VocabularyRepository:
 
         vocabulary[word] = word_details
 
-        self.save_vocabulary(vocabulary)
-
+        self.save_vocabulary(user_id, vocabulary)
         logger.info("Word added to vocabulary: %s", word)
+
+    def _create_new_vocabulary_file(self, user_id):
+        empty_vocab = {}
+        key = self._create_key(user_id)
+        try:
+            self.s3_client.put_object(
+                Bucket=self.bucket_name,
+                Key=key,
+                Body=json.dumps(empty_vocab).encode('utf-8'),
+                ContentType='application/json'
+            )
+            logger.info("Created new vocabulary file for user %s", user_id)
+        except ClientError as put_error:
+            logger.error("Failed to create vocabulary file: %s", put_error)
+            raise
+        return empty_vocab
+
+    def _create_key(self, user_id: str):
+        return f"{user_id}{self.USER_VOCABULARY_FILE_SUFFIX}"
--- daily_dragon/service/vocabulary_service.py
+++ daily_dragon/service/vocabulary_service.py
@@ -13,20 +13,20 @@ class VocabularyService:
     def __init__(self, vocabulary_repository: VocabularyRepository = Depends()):
         self.vocabulary_repository = vocabulary_repository
 
-    def add_word(self, word):
-        return self.vocabulary_repository.add_word(word)
+    def add_word(self, user_id: str, word: str):
+        return self.vocabulary_repository.add_word(user_id, word)
 
-    def get_vocabulary(self):
-        return self.vocabulary_repository.get_vocabulary()
+    def get_vocabulary(self, user_id: str):
+        return self.vocabulary_repository.get_vocabulary(user_id)
 
-    def delete_word(self, word):
-        vocabulary = self.vocabulary_repository.get_vocabulary()
+    def delete_word(self, user_id: str, word: str):
+        vocabulary = self.vocabulary_repository.get_vocabulary(user_id)
         if word in vocabulary:
             del vocabulary[word]
-            self.vocabulary_repository.save_vocabulary(vocabulary)
+            self.vocabulary_repository.save_vocabulary(user_id, vocabulary)
         logger.info(f"Deleted word {word}")
 
-    def get_random_vocabulary(self, count):
-        all_vocabulary = self.vocabulary_repository.get_vocabulary()
+    def get_random_vocabulary(self, user_id: str, count: int):
+        all_vocabulary = self.vocabulary_repository.get_vocabulary(user_id)
         random_words = random.sample(list(all_vocabulary.keys()), min(count, len(all_vocabulary)))
         return {word: all_vocabulary[word] for word in random_words}
